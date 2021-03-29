45f14f48f969afde516fb74cac1175848392c3cc
Vivek Panchal <vivekpanchal@Viveks-MacBook-Pro.local>
2021-03-29 14:31:10 -0400

    Changed breather excercise to 1min

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
-const totalTime = 7500;
+const totalTime = 60000;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
